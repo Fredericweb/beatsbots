db0320548f3495b25425d018e1ffa4dedc8c63b6
AsteroBlack <93392469+AsteroBlack@users.noreply.github.com>
2022-07-14 21:36:56 +0000

    first

--- bot.js
+++ bot.js
@@ -16,19 +16,20 @@ const msgHelp = `
     /help - pour plus d'info sur les commande du BOT
     /echo - pour retourner un mot à l'ecran (/echo jeux retourne jeux )
 `;
-const web_link = "https://beatsmarket.herokuapp.com/";
+const web_link = "https://formappmessenka.herokuapp.com/";
 // const link2 ="https://celebrated-torte-184681.netlify.app/"
 // const link1 = "https://mproweb.uz/YTless/greenMarket/store/"
 
-bot.start((ctx) =>
-  ctx.reply("Bienvenu sur beats markert", {
+bot.start((ctx) =>{
+  // bot.telegram.sendPhoto(ctx.chat.id,"https://upload.wikimedia.org/wikipedia/commons/7/77/Google_Images_2015_logo.svg")
+  ctx.reply("Bienvenu sur zowblazo bot", {
     reply_markup: {
-      inline_keyboard: [[{ text: "💳Achetez maintenant💳", web_app: { url: web_link } }
-
+      inline_keyboard: [[{ text: "Nouveau formulaire Messenka", web_app: { url: web_link } }
     ]]
-      
     },
-  }),
+  })
+}
+ 
 );
 
 bot.help((ctx) => {
@@ -40,55 +41,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-app.get('/' ,(req,res)=>{
-	res.send('')
-})
-
-
-app.post("/click", (req, res) => {
-	let { total, user } = req.body
-  let tab =[]
-  tab.push(total)
-  console.log(tab)
-  
-    const getInvoice =  (e) => {
-      const invoice ={
-        title : "paiement",
-        description: "Casque Beats",
-        payload:"A450-03",
-        // start_parameter: 'online_conslutation',
-        provider_token:"284685063:TEST:ZTRkOWZkZjRlNTgy",
-        currency:"USD",
-        photo_url:"https://mproweb.uz/YTless/greenMarket/store/img/pay.jpg",
-        photo_width:735,
-        photo_heigth:490,
-        need_name : true,
-        need_phone_number : true,
-        need_email : true,
-        need_shipping_address : true,
-        send_phone_number_to_provider : true,
-        is_flexible: false,
-        prices: [{label : 'Casques Beats' , amount : e*100}],
-        reply_markup: {
-            inline_keyboard: [[{ text: "payez "+e +"$US", pay:true }]] 
-        }
-
-      
-    }
-      return invoice
-    }
-    bot.telegram.sendInvoice(user.id,getInvoice(total)
-    )
-    bot.use(Telegraf.log())
-    bot.on('pre_checkout_query', (ctx) => ctx.answerPreCheckoutQuery(true)) //      
-
-    bot.on('successful_payment', async (ctx, next) => { //     
-      await ctx.reply('Paiement effectuée avec succès 👍👍 !!')
-    })
-
-    res.send('ok'+total)
-
-})
 app.listen(port, () => {
 	console.log(`Listening at http://localhost:${port}`)
 })
